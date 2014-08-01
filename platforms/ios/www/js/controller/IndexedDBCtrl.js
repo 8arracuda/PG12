c2f32c725e67d9d7b182ddb3e320a40ae6d34b02
steinermichael <michaelsteiner1985@googlemail.com>
2014-08-01 16:40:07 +0200

    -added basic functions for indexedDB (mainly from the mozilla example page)

--- platforms/ios/www/js/controller/IndexedDBCtrl.js
+++ platforms/ios/www/js/controller/IndexedDBCtrl.js
@@ -1,4 +1,18 @@
 sdApp.controller('IndexedDBCtrl', function ($scope) {
 
 
+    $scope.openDatabase = function () {
+        console.log('openDatabase start');
+
+        if (!window.indexedDB) {
+            window.alert("Ihr Browser unterstützt keine stabile Version von IndexedDB. Dieses und jenes Feature wird Ihnen nicht zur Verfügung stehen.");
+        } else {
+            
+            // Öffnen unserer Datenbank
+            var request = window.indexedDB.open("MeineTestdatenbank", 3);
+            console.log('openDatabase end');
+
+        }
+    };
+
 });
